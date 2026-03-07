ee3e3d7fb793c8623f08831408c6bdaa9ddae0ee
NikoSka82 <scaltrito.domenico@gmail.com>
2026-03-07 08:28:09 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,11 @@
-self.addEventListener('install', (e) => { e.waitUntil(Promise.resolve()); }); self.addEventListener('fetch', (e) => { e.respondWith(fetch(e.request)); });
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('stipendio-v1').then((cache) => cache.addAll(['./index.html']))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
